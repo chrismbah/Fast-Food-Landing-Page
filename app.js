a2519e368e46b6742768faec9c81ee27e736f04b
Chris Mbah <mbahchris46@gmail.com>
2023-06-17 19:35:41 +0100

    slight changes

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 let menu=document.querySelector("#menu-icon");
+const mode=document.querySelector("#toggle-icon");
+mode.onclick=function(){
+    body=document.querySelector(".light");
+    body.classList.toggle("dark"); 
+}
 menu.onclick=function(){
     navBar =document.querySelector(".navbar");
     navBar.classList.toggle("open")
